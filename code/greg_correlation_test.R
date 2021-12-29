1785359824ac9210355a32f278bba04c0e281094
Quang Nguyen <ngminhquang.99@gmail.com>
2021-12-28 22:24:25 -0600

    only sim section left

--- code/greg_correlation_test.R
+++ code/greg_correlation_test.R
@@ -1,8 +1,8 @@
-#Desired Spearman correlation between bouldering and lead
+# Desired Spearman correlation between bouldering and lead
 rho <- 0.5
-set.seed(1234)
+set.seed(1999)
 
-#Step 1: Generate the marginals with nsim = 100000
+# Step 1: Generate the marginals with nsim = 100000
 nsim <- 100000
 speed <- runif(nsim)
 lead <- runif(nsim)
@@ -10,27 +10,24 @@ bouldering <- runif(nsim)
 
 dat <- data.frame(speed, lead, bouldering)
 
-#Step 2.  The upper and lower bounds are 1 and -1.  
-#Don't need this here. 
+# Step 2.  The upper and lower bounds are 1 and -1.
+# Don't need this here.
 
-#Step 3.
-#Pick some indexes
+# Step 3.
+# Pick some indexes
 ind <- sample(1:nsim, rho * nsim, replace = FALSE)
 dat$lead[ind] <- sort(dat$lead[ind])
 dat$bouldering[ind] <- sort(dat$bouldering[ind])
+cor(dat, method = "spearman")
 
-#Now we have the "population" that has the desired correlation.  
+# Now we have the "population" that has the desired correlation.
 test <- c()
-for (i in 1:100000){print(i)
-ind2 <- sample(1:nsim, 20, replace = FALSE)
-prelims <- dat[ind2,]
-prelims <- as.data.frame(apply(prelims, 2, rank))
-test[i] <- cor(prelims$lead,prelims$bouldering, method = "spearman")
+for (i in 1:10000) {
+  ind2 <- sample(1:nsim, 20, replace = FALSE)
+  prelims <- dat[ind2, ]
+  prelims <- as.data.frame(apply(prelims, 2, rank))
+  test[i] <- cor(prelims$lead, prelims$bouldering, method = "spearman")
 }
 
 hist(test)
-
-
-
-
-
+mean(test)
